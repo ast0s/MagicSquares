026580f96625c7a4684396d3ec63280fb30d248e
BulatNick <bulatnikov.mykyta@vu.cdu.edu.ua>
2023-12-05 12:02:29 +0200

    done

--- MagicSquares/MainWindow.xaml.cs
+++ MagicSquares/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MagicSquares
 {
@@ -20,9 +10,127 @@ namespace MagicSquares
     /// </summary>
     public partial class MainWindow : Window
     {
+        int[,] magicSquare;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void encrypt_button_Click(object sender, RoutedEventArgs e)
+        {
+            int n = int.Parse(matrix_n.Text);
+            string text = text_box.Text;
+
+            if (text.Length % (n * n) != 0)
+            {
+                MessageBox.Show("The text length must be a multiple of the matrix size!");
+                return;
+            }
+
+
+            StringBuilder encryptedMessage = new StringBuilder();
+
+            for (int k = 0; k < text.Length / (n*n); k++)
+            {
+                int i = 0;
+
+                for (; i < n; i++)
+                {
+                    StringBuilder encryptedRow = new StringBuilder();
+
+                    for (int j = 0; j < n; j++)
+                    {
+                        encryptedRow.Append(text[k * n * n + magicSquare[i, j] - 1]);
+                    }
+                    encryptedMessage.Append(encryptedRow);
+                }
+            }
+            
+            result_text.Text = encryptedMessage.ToString();
+
+        }
+
+        private void decrypt_button_Click(object sender, RoutedEventArgs e)
+        {
+            int n = int.Parse(matrix_n.Text);
+            string text = text_box.Text;
+
+            if (text.Length % (n * n) != 0)
+            {
+                MessageBox.Show("The text length must be a multiple of the matrix size!");
+                return;
+            }
+
+            char[] decryptedMessage = new char[text.Length];
+
+            for (int k = 0; k < text.Length / (n * n); k++)
+            {
+                int i = 0;
+
+                for (; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        decryptedMessage[k * n * n + magicSquare[i, j] - 1] = text[k * n * n + i * n + j];
+                    }
+                }
+            }
+
+            result_text.Text = new string(decryptedMessage);
+        }
+
+        private void generate_button_Click(object sender, RoutedEventArgs e)
+        {
+            int n = int.Parse(matrix_n.Text);
+
+            magicSquare = GenerateMatrix(n);
+            PrintMatrix(magicSquare, n);
+        }
+
+        private int[,] GenerateMatrix(int n)
+        {
+            Random random = new Random();
+
+            int[,] matrix = new int[n, n];
+            int[] numbers = new int[n*n];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = i + 1;
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int temp = numbers[i];
+                int randomIndex = random.Next(i, numbers.Length);
+                numbers[i] = numbers[randomIndex];
+                numbers[randomIndex] = temp;
+            }
+
+            int count = 0;
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = numbers[count];
+                    count++;
+                }
+            }
+
+            return matrix;
+        }
+
+        private void PrintMatrix(int[,] matrix, int n)
+        {
+            matrix_text.Text = "";
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    matrix_text.Text += matrix[row, col] + " ";
+                }
+                matrix_text.Text += "\n";
+            }
         }
     }
 }
